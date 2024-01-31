676a0c3571c11f4d81ba64cecc702cb01381c07e
todoggler <kentokuraCS@gmail.com>
2024-01-31 19:01:58 +0900

    test: 不足しているデモテストコードを追加

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,20 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
+import 'package:dart_mutation_test_example/point_service.dart';
+import 'package:dart_mutation_test_example/user.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dart_mutation_test_example/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group("60歳未満", () {
+    test('59歳の場合、100円で購入した場合は1ポイント付与される', () {
+      expect(PointService.point(100, User(name: "", age: 59)), equals(1));
+    });
+  });
+  group("60歳以上", () {
+    test('60歳の場合、100円で購入した場合は2ポイント付与される', () {
+      expect(PointService.point(100, User(name: "", age: 60)), equals(2));
+    });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    test('60歳の場合、10000円で購入した場合は200ポイント付与される', () {
+      expect(PointService.point(10000, User(name: "", age: 60)), equals(200));
+    });
   });
 }
